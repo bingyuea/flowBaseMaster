0d0615932d67bf7c76c78a31169bd620afc007ce
2310179201@qq.com <s2310179201>
2021-07-22 13:57:11 +0800

    feat：11.击短路计算，应该能弹出三种标准，但是还没做

--- src/Editor/containers/PanelRight.vue
+++ src/Editor/containers/PanelRight.vue
@@ -5,76 +5,98 @@
 */
 
 <style scoped lang="less" rel="stylesheet/less">
-  .panelRight {
-    width: 100px;
-    right: 0px;
-    background: #fff;
-    overflow: hidden;
-    box-sizing: border-box;
-    display: inline-block;
-    position: absolute;
-    bottom: 0;
-    top: 68px;
-    -webkit-box-shadow: 0 0 2px 2px rgb(0 0 0 / 10%);
-    z-index: 200;
-    -webkit-transition: all 0.5s ease-in-out;
-    transition: all 0.5s ease-in-out;
-    box-shadow: 0px 0px 10px 5px;
-    padding: 10px;
+.panelRight {
+  width: 100px;
+  right: 0px;
+  background: #fff;
+  overflow: hidden;
+  box-sizing: border-box;
+  display: inline-block;
+  position: absolute;
+  bottom: 0;
+  top: 68px;
+  -webkit-box-shadow: 0 0 2px 2px rgb(0 0 0 / 10%);
+  z-index: 200;
+  -webkit-transition: all 0.5s ease-in-out;
+  transition: all 0.5s ease-in-out;
+  box-shadow: 0px 0px 10px 5px;
+  padding: 10px;
 
-    &_title {
-      font-size: 16px;
-      text-align: center;
-      border-bottom: 1px solid #000000;
+  &_title {
+    font-size: 16px;
+    text-align: center;
+    border-bottom: 1px solid #000000;
+  }
+
+  &_content {
+    height: calc(100% - 30px);
+    overflow-y: auto;
+
+    &::-webkit-scrollbar-thumb {
+      border-radius: 4px;
+      -webkit-box-shadow: inset 0 0 5px rgb(78 78 78 / 20%);
+      background: hsla(0, 0%, 79%, .8);
     }
 
-    &_content {
-      height: calc(100% - 30px);
-      overflow-y: auto;
+    &::-webkit-scrollbar-track {
+      background: transparent;
+    }
 
-      &::-webkit-scrollbar-thumb {
-        border-radius: 4px;
-        -webkit-box-shadow: inset 0 0 5px rgb(78 78 78 / 20%);
-        background: hsla(0, 0%, 79%, .8);
-      }
+    &_item {
+      display: flex;
+      align-items: center;
+      flex-direction: column;
+    }
 
-      &::-webkit-scrollbar-track {
-        background: transparent;
-      }
+    .toolbarIcon {
+      width: 35px;
+      height: 35px;
+      margin-top: 15px;
+      padding: 2px;
+      background-size: cover;
+      border: 1px solid transparent;
 
-      &_item {
-        display: flex;
-        align-items: center;
-        flex-direction: column;
-      }
-
-      .toolbarIcon {
-        width: 35px;
-        height: 35px;
-        margin-top: 15px;
-        padding: 2px;
-        background-size: cover;
-        border: 1px solid transparent;
-
-        &:hover {
-          border-color: rgba(0, 0, 0, .1);
-          cursor: pointer;
-        }
+      &:hover {
+        border-color: rgba(0, 0, 0, .1);
+        cursor: pointer;
       }
     }
   }
+}
 </style>
 
 <template>
   <div class='panelRight' v-show="title">
-    <div class='panelRight_title'>{{title}}</div>
+    <div class='panelRight_title'>{{ title }}</div>
     <div class="panelRight_content">
 
       <div v-if='toolbarIcon' class="panelRight_content_item">
-        <img @click='handleCalc(item)' v-for='(item,key) in toolbarIcon' :key='key' class='toolbarIcon'
-             :title="item.icon"
-             :src='item.icon ? require(`../../assets/images/toolbar/${item.icon}.png`) : ""'
-             alt=""/>
+        <div v-for='(item,key) in toolbarIcon' :key='key'>
+
+
+          <div v-if="item.parent === '短路计算' && item.icon === '三相接地短路'">
+            <el-dropdown @command='(command) => handleCalc(item,command)'>
+              <span class="el-dropdown-link">
+                <img class='toolbarIcon'
+                     :title="item.icon"
+                     :src='item.icon ? require(`../../assets/images/toolbar/${item.icon}.png`) : ""'
+                     alt=""/>
+              </span>
+              <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item command='CCS'>CCS标准</el-dropdown-item>
+                <el-dropdown-item command='GJB'>GJB标准</el-dropdown-item>
+                <el-dropdown-item command='GB'>GB标准</el-dropdown-item>
+              </el-dropdown-menu>
+            </el-dropdown>
+          </div>
+
+          <img v-else @click='handleCalc(item)' class='toolbarIcon'
+               :title="item.icon"
+               :src='item.icon ? require(`../../assets/images/toolbar/${item.icon}.png`) : ""'
+               alt=""/>
+        </div>
+
+
       </div>
     </div>
   </div>
@@ -110,11 +132,11 @@
         const _t = this
         _t.$X.utils.bus.$emit('editor/panel/toggle', data)
       },
-      handleCalc (item) {
+      handleCalc (item,command) {
         /* if (item.icon === '三相接地短路') {
-          // 上传
-          this.$emit('show', 'uploadShow')
-        } */
+         // 上传
+         this.$emit('show', 'uploadShow')
+         } */
         if (item.parent === '短路计算' && item.icon === '生成结果报告') {
           this.$emit('show', 'resultShow')
         }
@@ -127,7 +149,8 @@
             type: 'dropdown-list'
           })
         }
-        console.log(item)
+        // todo 不同的标准
+        console.log(item,command)
       }
     }
 
